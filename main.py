7614f6b4230e77b11378122a492ccd87aebc7ca8
Kikika-creator <74352278+Kikika-creator@users.noreply.github.com>
2021-03-14 11:11:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import PasswordField, StringField, SubmitField, IntegerField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat_password', validators=[DataRequired()])
+    surname = StringField("Surname", validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField("Age", validators=[DataRequired()])
+    position = StringField("Position", validators=[DataRequired()])
+    speciality = StringField("Speciality", validators=[DataRequired()])
+    address = StringField("Address", validators=[DataRequired()])
+    submit = SubmitField('Sumbit')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=int(form.age.data),
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
+    app.run(port=8080, host='127.0.0.1')
